557fdf4ab0d1829a8eb10bf693fb3dd6cb331580
ciatph <ciat.ph@gmail.com>
2018-05-02 10:34:45 +0700

    Added instructions for accessing remote API's.

--- application/views/cassava/index.php
+++ application/views/cassava/index.php
@@ -4,4 +4,7 @@ Get consolidated farmland data<br>.
 
 <ul>
 	<li><a href="<?php echo base_url(); ?>cassava/export">Export Data to CSV</a></li>
+	<li>Get data from a remote API (using internal scripts)<br>
+		<?php echo base_url(); ?>cassava/getremote?url=ENTER_RESTFUL_API_URL_HERE
+	</li>
 </ul>
